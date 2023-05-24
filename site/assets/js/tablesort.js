45391ec18fc0c54d8aacbde475513e750a0b4614
Anill Maraj <103867764+AnillMaraj@users.noreply.github.com>
2023-05-24 02:26:47 -0400

    Initial Commit

---
+++ site/assets/js/tablesort.js
@@ -0,0 +1,6 @@
+﻿document$.subscribe(function () {
+    var tables = document.querySelectorAll("article table:not([class])")
+    tables.forEach(function (table) {
+        new Tablesort(table)
+    })
+})
